081bd77f3bfd986b7a5ac0fa67978382ed94c1ec
Emil Sågfors <emil.sagfors@iki.fi>
2014-08-30 11:25:00 +0300

    Add Recon 0.251a

---
+++ Casks/recon.rb
@@ -0,0 +1,9 @@
+class Recon < Cask
+  version '0.251a'
+  sha256 '48fbae8a2d2b14409e38bc05e61ba67f61e5c776718cb5250cc4cbf30fc78a29'
+
+  url 'https://recon-mac.googlecode.com/files/Recon-0.251a.dmg'
+  homepage 'https://code.google.com/p/recon-mac/'
+
+  link 'recon.app'
+end
